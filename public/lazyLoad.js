088ce9b30332104568ef3f65e80c452c8e5bf9c2
Vincent du Plessis <vinduples@gmail.com>
2022-01-21 19:27:34 +0200

    Removed all nodelist apending code

--- public/lazyLoad.js
+++ public/lazyLoad.js
@@ -32,38 +32,15 @@ function getData() {
         // var postTemplate 
           let src = `/memes/${data[filenameCounter + i].fileName}`;
           let videoTemplate = `
-        
           <div class="container ">
             <h6><img src="" alt="" width="20px">Funny </h6>
             <h3>This is a cool meme</h3>
             <video controls preload="auto" loop="" playsinline src="${src}" alt="a meme" class="rounded">
-            <source src="${src}" type="video/mp4; codecs=&quot;av01.0.00M.08, opus&quot;"><!----><!----><source src="${src}" type="video/mp4"><!---->
+            <source src="${src}" type="video/mp4; codecs=&quot;av01.0.00M.08, opus&quot;">
+            <source src="${src}" type="video/mp4">
             </video>
           </div> 
-  
-          `
-
-          let div = document.createElement('div');
-          div.classList.add('container');
-          let h6 = document.createElement('h6');
-          h6.innerText = 'Funny';
-          div.append(h6);
-          let h3 = document.createElement('h3');
-          h3.innerText = 'This is a cool meme';
-          div.appendChild(h3);
-          let video = document.createElement('video');
-          video.setAttribute('controls', '');
-          video.setAttribute('loop', '');
-        video.setAttribute('playsinline', '');
-          video.classList.add('rounded')
-          video.src = src;
-          video.type="video/mp4"
-          video.setAttribute('alt', 'a meme');
-          video.setAttribute('width', '90%');
-          // video.addEventListener('mouseover', play);
-          // video.addEventListener('touchstart', playM)
-          // video.addEventListener('mouseleave', pause);
-          div.appendChild(video);
+          `;
           feed.innerHTML += videoTemplate;       
           if(i == 1){
           let observertarget = document.createElement('observertarget');

ab19a1d5b6613aa8feffd929adfac60165eec748
Vincent du Plessis <vinduples@gmail.com>
2022-02-10 12:16:44 +0200

    using env vars and baucketname to s3 class

--- index.js
+++ index.js
@@ -12,15 +12,16 @@ const path = require('path');
 const S3 = require('aws-sdk/clients/s3');
 const fs = require('fs');
 
-const bucketName = "memeitboyz";
-const region = "eu-west-1";
-const accessKeyId = "Y3D26hQKe1uB3aqCCk79vz6L71FkZ8nGB5i7KYnq"
-const secretAccesskey = "AKIA4MQP2Q7N3N4CROHU"
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccesskey = process.env.AWS_ACCESS_KEY_SECRET;
 
 const s3 = new S3({
 	region,
 	accessKeyId,
-	secretAccesskey
+	secretAccesskey,
+	bucketName
 })
 // Create express app
 const app = express();
